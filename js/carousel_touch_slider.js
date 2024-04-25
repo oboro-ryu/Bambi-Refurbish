8a9b4d1ea52b3c9bfb6233f22ac7f4372f9f31fd
oboro <my.ancestor.skanderbeg@gmail.com>
2024-04-25 18:36:11 +0900

    コミットメッセージ 新しい中古家電屋のオンラインショップ
    git remote add origin

---
+++ js/carousel_touch_slider.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    var $carousel = $('.carousel');
+    $carousel.on('touchstart', function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one('touchmove', function(event){
+            var xMove = event.originalEvent.touches[0].pageX;
+            if( Math.floor(xClick - xMove) > 5 ){
+                $(this).carousel('next');
+            }
+            else if( Math.floor(xClick - xMove) < -5 ){
+                $(this).carousel('prev');
+            }
+        });
+        $carousel.on('touchend', function(){
+            $(this).off('touchmove');
+        });
+    });
+});
